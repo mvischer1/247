6ea047b6137ea5decd2055812e0f7d85148904ec
mvischer1 <59977016+mvischer1@users.noreply.github.com>
2020-01-21 10:39:39 -0500

    Update WalkBehavior.java

--- WalkBehavior.java
+++ WalkBehavior.java
@@ -1,5 +1,5 @@
-public class WalkBehavior implements MoveBehavior {
-  public String move() {
+public class WalkBehavior implements MoveBehavior {  //Used for the HumanRobot class
+  public String move() {  //implents the move method from MoveBehavior and returns a unique string
     return "Walking Gleefully";
   }
 }
