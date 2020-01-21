35c5f494cc2217acc031d39fb474adc429d41130
mvischer1 <59977016+mvischer1@users.noreply.github.com>
2020-01-21 09:28:52 -0500

    Update CrawlBehavior.java

--- CrawlBehavior.java
+++ CrawlBehavior.java
@@ -1,5 +1,6 @@
-public class CrawlBehavior implements MoveBehavior {
-  public String move() {
+public class CrawlBehavior implements MoveBehavior {  //Used for the DogRobot Class
+  
+  public String move() {  //Implements the move method from the MoveBehavior interface and returns a unique String
     return "Crawling Playfully";
   }
 }
