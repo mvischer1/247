cfabbe9951a0ae71c940462941896a47b71e8bce
mvischer1 <59977016+mvischer1@users.noreply.github.com>
2020-01-21 09:43:15 -0500

    Update Robot.java

--- Robot.java
+++ Robot.java
@@ -1,28 +1,29 @@
 public abstract class Robot {
   
+  //Declare attributes each Robot will have
   protected String name;
   MoveBehavior moveBehavior;
   TalkBehavior talkBehavior;
   
-  public Robot(String aName) {
+  public Robot(String aName) {  //Construct Robot
     this.name = aName;
   }
   
-  public void setMoveBehavior(MoveBehavior aMoveBehavior) {
+  public void setMoveBehavior(MoveBehavior aMoveBehavior) {  //Mutator that provides a way to change how a robot moves
     this.moveBehavior = aMoveBehavior;
   }
   
-  public void setTalkBehavior(TalkBehavior aTalkBehavior) {
+  public void setTalkBehavior(TalkBehavior aTalkBehavior) {  //Mutator that provides a way to change how a robot talks
     this.talkBehavior = aTalkBehavior;
   }
   
-  public String move() {
+  public String move() {  //returns the specific Strings that are established in the sub classes
     return moveBehavior.move();
   }
   
-  public String talk() {
+  public String talk() {  //returns the specific Strings that are established in the sub classes
     return talkBehavior.talk();
   }
   
-  public abstract String toString();
+  public abstract String toString();  //Abstract method that is to be implemented in each Robot sub class
 }
