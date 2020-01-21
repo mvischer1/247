498cc80cd6c5868d7fa67d2343ebae5494d80b61
mvischer1 <59977016+mvischer1@users.noreply.github.com>
2020-01-21 10:41:13 -0500

    Update WallERobot.java

--- WallERobot.java
+++ WallERobot.java
@@ -1,12 +1,12 @@
 public class WallERobot extends Robot {
   
-  public WallERobot(String aName) {
-    super(aName);
+  public WallERobot(String aName) {  //Constructor for the WallERobot, sets move and talk behaviors to correct type
+    super(aName); //Calls the constructor from the base class Robot
     moveBehavior = new WalkBehavior();
     talkBehavior = new SimpleSpeakBehavior();
   }
   
-  public String toString() {
+  public String toString() {  //Implemtation of the abstract method toString from the Robot class, returns specific String
     return name+" is an environmentally friendly Wall-E Robot";
   }
 }
