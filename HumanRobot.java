bd34e9b91810ab4ed67f533179dbc20c27ab8a5b
mvischer1 <59977016+mvischer1@users.noreply.github.com>
2020-01-21 09:36:45 -0500

    Update HumanRobot.java

--- HumanRobot.java
+++ HumanRobot.java
@@ -1,12 +1,12 @@
 public class HumanRobot extends Robot {
   
-  public HumanRobot(String aName) {
-    super(aName);
+  public HumanRobot(String aName) {  //Constructs HumanRobot
+    super(aName);  //Calls constructor from Robot
     moveBehavior = new WalkBehavior();
     talkBehavior = new SpeakBehavior();
   }
   
-  public String toString() {
+  public String toString() {  //implements the abstract method toString from the class Robot
     return name+" is a typical human robot";
   }
 }
