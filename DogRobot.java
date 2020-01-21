0e455012ec188b145baf22e7c6dd95338c6985cc
mvischer1 <59977016+mvischer1@users.noreply.github.com>
2020-01-21 09:33:26 -0500

    Update DogRobot.java

--- DogRobot.java
+++ DogRobot.java
@@ -1,12 +1,12 @@
 public class DogRobot extends Robot {
   
-  public DogRobot(String aName) {
-    super(aName);
-    moveBehavior = new CrawlBehavior();
-    talkBehavior = new BarkBehavior();
+  public DogRobot(String aName) {  //Constructor for a DogRobot object
+    super(aName);  //Calls constructor from Robot
+    moveBehavior = new CrawlBehavior();  //Assigns moveBehavior attribute to the specific type
+    talkBehavior = new BarkBehavior();  //Assigns talkBehavior attribute to the specific type
   }
   
-  public String toString() {
+  public String toString() {  //Implements the abstract method toString from Robot class, and returns unique String
     return name+" is a happy dog robot";
   }
 }
