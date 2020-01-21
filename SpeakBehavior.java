78e2fc5a66d67d742ac358c343da843c45520062
mvischer1 <59977016+mvischer1@users.noreply.github.com>
2020-01-21 10:36:35 -0500

    Update SpeakBehavior.java

--- SpeakBehavior.java
+++ SpeakBehavior.java
@@ -1,9 +1,10 @@
-import java.util.Random;
+import java.util.Random;  //Imports the java class Random to allow the program to create a RNG
 
 public class SpeakBehavior implements TalkBehavior {
-  Random r = new Random();
-  public String talk() {
-    int i = r.nextInt(3)+1;
+  Random r = new Random();  //initializes the type Random
+  
+  public String talk() {  //Implements the talk method from the TalkBehavior interface
+    int i = r.nextInt(3)+1;  //Generates a number between 1-3 in order to randomly select a phrase
     if(i == 1)
       return "Hello";
     else if(i == 2)
