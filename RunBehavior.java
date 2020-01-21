b8e318f5c134b9ab23e1568f5877bff52a7b5ae6
mvischer1 <59977016+mvischer1@users.noreply.github.com>
2020-01-21 09:45:24 -0500

    Update RunBehavior.java

--- RunBehavior.java
+++ RunBehavior.java
@@ -1,5 +1,5 @@
-public class RunBehavior implements MoveBehavior {
-  public String move() {
+public class RunBehavior implements MoveBehavior {  //MoveBehavior implementation for the iRobot class
+  public String move() {  //Implements the move method from the MoveBehavior interface and returns a unique String
     return "Running super agressivelly!!! AAAAHHHHH";
   }
 }
