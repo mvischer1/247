a9e35319a045065e1d3846c1ffba1e8862639141
mvischer1 <59977016+mvischer1@users.noreply.github.com>
2020-01-21 10:34:28 -0500

    Update SimpleSpeakBehavior.java

--- SimpleSpeakBehavior.java
+++ SimpleSpeakBehavior.java
@@ -1,10 +1,10 @@
-import java.util.Random;
+import java.util.Random;  //Imports the Random class allowing program to implement a RNG
 
 public class SimpleSpeakBehavior implements TalkBehavior {
-  Random r = new Random();
+  Random r = new Random();  //creates a type random to be used in the talk class
   
-  public String talk() {
-    int i = r.nextInt(5)+1;
+  public String talk() {  //Implementing the talk merthod from the TalkBehavior interface
+    int i = r.nextInt(5)+1;  //Generates a random number between 1-5 to pick which phrase the robot will use
     if(i == 1)
       return "Wall-E";
     else if(i == 2)
