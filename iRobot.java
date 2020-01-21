27e8593918d7972d283f79f94db12b8b35698996
mvischer1 <59977016+mvischer1@users.noreply.github.com>
2020-01-21 10:42:35 -0500

    Update iRobot.java

--- iRobot.java
+++ iRobot.java
@@ -1,12 +1,12 @@
 public class iRobot extends Robot {
   
-  public iRobot(String aName) {
-    super(aName);
+  public iRobot(String aName) {  //Constructor for the iRobot object
+    super(aName);  //Calls the constructor from the parent class robot
     moveBehavior = new RunBehavior();
     talkBehavior = new SpeakBehavior();
   }
   
-  public String toString() {
+  public String toString() {  //Implements the toString abstract method from the Robot class and returns a unique String
     return name+" is a scary iRobot";
   }
 }
